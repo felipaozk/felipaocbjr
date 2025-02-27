5ac2105253905ea60595e179a6279e8b1a417fbe
felipaozk <161710165+felipaozk@users.noreply.github.com>
2025-02-27 11:22:43 -0300

    tememe

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const diminuiFonteBotao = document.getElementById(diminuir-fonte);
 let tamanhoAtualFonte = 1;
 
 aumentaFonteBotao.addEventListener('click', function(){
-tamanhoAtualFonte += 0,1;
+tamanhoAtualFonte += 0.1;
 document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
 })
 diminuiFonteBotao.addEventListener('click', function(){
-    tamanhoAtualFonte -= 0,1;
+    tamanhoAtualFonte -= 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
 })
